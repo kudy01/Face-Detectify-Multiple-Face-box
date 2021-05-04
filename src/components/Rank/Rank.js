b2e3f9edb0dafb1f10bbc310c02f8f0fc33c46e6
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2021-05-04 21:48:39 +0530

    Added a ranker which uses AWS Lambda and serverless to rank users

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+class Rank extends Component {
+  constructor() {
+    super()
+    this.state = {
+      emoji: ''
+    }
+  }
 
-const Rank = ({name, entries}) => {
-	return (
-		<div>
-			<div className='white f3 '>
-				{`${name.charAt(0).toUpperCase() + name.slice(1)}, your current image detect count is`}
-			</div>
-			<div className='white f1 '>
-				{entries}
-			</div>
-		</div>	
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
 
-		)
+  componentDidUpdate(prevProps) {
+    if (prevProps.entries === this.props.entries) {
+      return null
+    }
+    this.generateEmoji(this.props.entries)
+  }
+
+  generateEmoji = (count) => {
+    fetch(`https://l56p8j01ie.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${count}`)
+      .then(response => response.json())
+      .then(data => this.setState({emoji: data.input}))
+      .catch(console.log)
+  }
+
+  render() {
+    const { name, entries } = this.props;
+    return (
+      <div>
+        <div className='white f3'>
+          {`${name}, your current entry count is...`}
+        </div>
+        <div className='white f1'>
+          {entries}
+        </div>
+        <div className= 'white f3'>
+          {`Rank badge: ${this.state.emoji}`}
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default Rank;
